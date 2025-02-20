accb293fedffcc135b76328f8a0968507c4608ed
BugHunter-X <askzenai@gmail.com>
2025-02-20 04:24:05 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,24 @@
+```javascript
+import React, { useState, useEffect } from 'react';
+
+function MyComponent() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      // Correct logic: Functional updates prevent race conditions
+      setCount(prevCount => prevCount + 1);
+    }, 1000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  return (
+    <div>
+      <p>Count: {count}</p>
+    </div>
+  );
+}
+
+export default MyComponent;
+```
